a2708c168bd7cc8d242cf4178d307050bb4c00a4
Joris Fonck <fonckjor@gmail.com>
2025-05-01 12:45:43 +0200

    footer font size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,8 +7,8 @@ const Footer: React.FC = () => {
     <footer className="py-8 text-center text-[#414042]/70 text-sm">
       <img src={Flower} className="w-full max-w-[1000px] mb-8 m-auto" />
       <p>Nous avons hâte de célébrer cet événement avec vous !</p>
-      <p className="mt-2">
-        <span className="font-serif italic">Juliette & Joris</span>
+      <p className="mt-4">
+        <span className="font-serif italic text-2xl">Juliette & Joris</span>
       </p>
     </footer>
   );
