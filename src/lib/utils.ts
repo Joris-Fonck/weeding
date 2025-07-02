5ca3e60e3243194ae7317e2123e849fc2b80c465
Joris Fonck <fonckjor@gmail.com>
2025-07-02 19:27:34 +0200

    fix: days

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -25,8 +25,7 @@ export function createUrlWithParams(path: string): string {
  * @returns A filtered schedule.
  */
 export function getFilteredSchedule(): DaySchedule[] {
-  const urlParams = new URLSearchParams(new URL(window.location.href).search);
-  const days = urlParams.getAll('days').map(Number).filter(day => day >= 1 && day <= 3);
+  const days = getDays()
 
   if (days.length === 0) {
     return SCHEDULE;
@@ -37,12 +36,33 @@ export function getFilteredSchedule(): DaySchedule[] {
 
 export function getFilteredDates(): string {
   const dates = [29, 30, 31]
-  const urlParams = new URLSearchParams(new URL(window.location.href).search);
-  const days = urlParams.getAll('days').map(Number).filter(day => day >= 1 && day <= 3);
+  const days = getDays()
 
   if (days.length === 0) {
     return dates.join(', ').replace(/, ([^,]*)$/, ' & $1');
   }
 
   return dates.filter((_, index) => days.includes(index + 1)).join(', ').replace(/, ([^,]*)$/, ' & $1');
+}
+
+function getDays() {
+  const urlParams = new URLSearchParams(new URL(window.location.href).search);
+
+  // Check local storage first
+  const storedDays = localStorage.getItem('days');
+  const debug = urlParams.get('debug');
+  if (storedDays && !debug) {
+    const days = storedDays.split(',').map(Number).filter(day => day >= 1 && day <= 3);
+    if (days.length > 0) {
+      return days;
+    }
+  }
+
+  const days = urlParams.getAll('days').map(Number).filter(day => day >= 1 && day <= 3);
+  if (days.length > 0) {
+    // Store the days in local storage for future use
+    localStorage.setItem('days', days.join(','));
+  }
+
+  return days;
 }
